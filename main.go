595a5a6960bca3e337a5181076f3b51bb5dfdbb9
Benjamin Chalmers <benj@minchalme.rs>
2018-08-21 18:45:05 +0100

    fixed

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/pe5er/radiotutor/quiz"
-	"strconv"
 )
 
 func main() {
@@ -12,49 +10,6 @@ func main() {
 
 	quiz.QuestionsInit()
 
-	score := 0
-	qnum := 0
-
-	for true {
-		fmt.Println("Score " + strconv.Itoa(score))
-		q := quiz.ReturnQuestion("M0", qnum)
-		printQuestion(q)
-		if checkAnswer(getAnswer(), q) {
-			fmt.Println("Welldone")
-			score += 1
-		} else {
-			fmt.Println("The answer is incorrect\n The correct answer is: " + returnAnswer(q))
-		}
-		qnum += 1
-	}
-
 	// Listen and Server in https://127.0.0.1:8080
-	//r.Run(":8080")
-}
-
-func printQuestion(q quiz.Question) {
-	fmt.Println(q.Question)
-	fmt.Println("1) " + q.Answers[0].Answer)
-	fmt.Println("2) " + q.Answers[1].Answer)
-	fmt.Println("3) " + q.Answers[2].Answer)
-	fmt.Println("4) " + q.Answers[3].Answer)
-}
-
-func getAnswer() int {
-	var i int
-	fmt.Scanf("%d", &i)
-	return i
-}
-
-func checkAnswer(i int, q quiz.Question) bool {
-	return q.Answers[i-1].Correct
-}
-
-func returnAnswer(q quiz.Question) string {
-	for _, a := range q.Answers {
-		if a.Correct {
-			return a.Answer
-		}
-	}
-	return ""
+	r.Run(":8080")
 }
