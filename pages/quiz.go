6c795007db2cc891c413c159664707e046440076
Benjamin Chalmers <benj@minchalme.rs>
2018-08-26 18:44:42 +0100

    Quiz is functional

--- pages/quiz.go
+++ pages/quiz.go
@@ -20,14 +20,16 @@ func QuizGet(c *gin.Context) {
 
 	q := session.Get(l + "Quiz").([]quiz.Question)
 	current := session.Get(l + "Current").(int)
-	c.HTML(200, "question.html", gin.H{
-		"Current":  strconv.Itoa(current + 1),
-		"NumQ":     strconv.Itoa(v.(int)),
-		"Question": q[current].Question,
-		"Answer1":  q[current].Answers[0].Answer,
-		"Answer2":  q[current].Answers[1].Answer,
-		"Answer3":  q[current].Answers[2].Answer,
-		"Answer4":  q[current].Answers[3].Answer})
+	if len(q) > current {
+		c.HTML(200, "question.html", gin.H{
+			"Current":  strconv.Itoa(current + 1),
+			"NumQ":     strconv.Itoa(v.(int)),
+			"Question": q[current].Question,
+			"Answer1":  q[current].Answers[0].Answer,
+			"Answer2":  q[current].Answers[1].Answer,
+			"Answer3":  q[current].Answers[2].Answer,
+			"Answer4":  q[current].Answers[3].Answer})
+	}
 
 	//session.Save()
 }
@@ -37,6 +39,7 @@ func QuizPost(c *gin.Context) {
 	session := sessions.Default(c)
 	l := c.Param("licenceType")
 
+	// Initial stage post
 	if numberOfQuizS, ok := c.GetPostForm("number"); ok {
 		numberOfQuiz, err := strconv.Atoi(numberOfQuizS)
 		if err != nil {
@@ -49,17 +52,52 @@ func QuizPost(c *gin.Context) {
 
 	v := session.Get(l + "Current")
 	if v == nil {
+		// Setup
 		session.Set(l+"Current", int(0))
 		correct := make([]bool, session.Get(l+"Started").(int))
 		session.Set(l+"Quiz", quiz.ReturnQuiz(l, 1, session.Get(l+"Started").(int)))
-		fmt.Println(session.Get(l + "Quiz"))
-		fmt.Println(quiz.ReturnQuestion("M0", 1))
-		fmt.Println(session.Get(l + "Started").(int))
-		fmt.Println(quiz.ReturnQuiz(l, 1, session.Get(l+"Started").(int)))
 		session.Set(l+"QuizCorrect", correct)
 	} else {
+		// Change question
+		q := session.Get(l + "Quiz").([]quiz.Question)
+		len := session.Get(l + "Started").(int)
 		current := v.(int)
-		current++
+		correctIndex := -1
+		correct := session.Get(l + "QuizCorrect").([]bool)
+
+		for i, ans := range q[current].Answers {
+			if ans.Correct {
+				correctIndex = i
+				break
+			}
+		}
+
+		fmt.Println(correctIndex)
+		ans, _ := c.GetPostForm("answer")
+		if i, _ := strconv.Atoi(ans); i == correctIndex {
+			fmt.Println("Correct")
+			correct[current] = true
+		} else {
+			fmt.Println("Wrong")
+			correct[current] = false
+		}
+
+		session.Set(l+"QuizCorrect", correct)
+		fmt.Println(ans)
+
+		if (len - 1) == current {
+			score := int(0)
+			for _, j := range correct {
+				if j {
+					score++
+				}
+			}
+			c.String(200, strconv.Itoa(score)+"/"+strconv.Itoa(len))
+			session.Clear()
+			return
+		} else {
+			current++
+		}
 		session.Set(l+"Current", current)
 	}
 
