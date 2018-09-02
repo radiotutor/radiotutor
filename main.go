80c2c2e18e50fa33672cfccd6a7fc18fd7d3a99f
Benjamin Chalmers <benj@minchalme.rs>
2018-09-02 17:37:09 +0100

    things

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/autotls"
+	"github.com/gin-gonic/gin"
+	"io"
+	"os"
+)
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
@@ -8,6 +14,17 @@ func main() {
 	r := routes()
 	r.LoadHTMLGlob("templates/*")
 
+	// Logging to a file.
 	// Listen and Server in https://127.0.0.1:8080
-	r.Run(":8080")
+	if os.Getenv("RADIOTUTOR_DEV") == "true" {
+		fmt.Printf("hello world")
+		r.Run(":8080")
+	} else {
+
+		gin.DisableConsoleColor()
+		f, _ := os.Create("gin.log")
+		gin.DefaultWriter = io.MultiWriter(f)
+
+		autotls.Run(r, "radiotutor.uk")
+	}
 }
