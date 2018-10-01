cd2c57b209135a3bca90a87b475f6e0591a3d2db
Benjamin Chalmers <benj@minchalme.rs>
2018-10-01 16:16:46 +0100

    Fix

---
+++ dbutils/dbutils.go
@@ -0,0 +1,31 @@
+package dbutils
+
+import (
+	"database/sql"
+	"errors"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/square/squalor"
+	"os"
+)
+
+var (
+	serverURL   = os.Getenv("SQL_URL")
+	sqlPassword = os.Getenv("SQL_PASSWORD")
+	sqlUsername = os.Getenv("SQL_USERNAME")
+)
+
+func DB(ex func(*squalor.DB) interface{}) interface{} {
+	_db, err := sql.Open("mysql", sqlUsername+":"+sqlPassword+"@tcp("+serverURL+")/rt")
+	if err != nil {
+		panic(err)
+	}
+	defer _db.Close()
+
+	db, _ := squalor.NewDB(_db)
+	if err != nil {
+		panic(err)
+	}
+
+	rtn := ex(db)
+	return rtn
+}
